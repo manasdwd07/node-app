5d8c563d7da18722344a53b90baa1cf36816ed55
manasdwd07 <manas.dwivedi@neosofttech.com>
2020-08-21 11:24:18 +0530

    first commit

---
+++ test/test.js
@@ -0,0 +1,9 @@
+var request = require('supertest');
+var app = require('../index');
+
+describe('GET /', function () {
+    it(`respond with hello world `, function (done) {
+        // navigate to root directory and check the response is "hello world"
+        request(app).get('/').expect(`hello world`, done);
+    });
+});
